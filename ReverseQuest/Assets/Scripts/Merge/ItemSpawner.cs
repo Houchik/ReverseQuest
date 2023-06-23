019b36351a1c183fb079b4f799f9207f1bf50252
lihtniht <114800709+Houchik@users.noreply.github.com>
2023-06-23 15:33:52 +0600

    spawner-fixed

--- ReverseQuest/Assets/Scripts/Merge/ItemSpawner.cs
+++ ReverseQuest/Assets/Scripts/Merge/ItemSpawner.cs
@@ -1,17 +1,49 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemSpawner : MonoBehaviour
 {
     [SerializeField] GameObject _itemToSpawn;
+
+    [SerializeField] private int _clicksToReload;
+    [SerializeField] private int _reloadTime;
+
+    private Image _image;
+    private Button _button;
+
     private int _clicksCounter;
+    private bool _needToReload;
+
+    private void Start()
+    {
+        _image = GetComponent<Image>();
+        _button = GetComponent<Button>();
+    }
+
+    private void Update()
+    {
+        if (_needToReload)
+        {
+            _image.fillAmount += 1.0f / _reloadTime * Time.deltaTime;
+
+            if (_image.fillAmount == 1)
+            {
+                _button.interactable = true;
+                _needToReload = false;
+            }
+        }
+    }
 
     public void OnSpawnerClicked()
     {
         _clicksCounter++;
 
-        if (_clicksCounter >= 3)
+        if (_clicksCounter == _clicksToReload)
         {
-            Destroy(gameObject);
+            _image.fillAmount = 0;
+            _clicksCounter = 0;
+            _button.interactable = false;
+            _needToReload = true;
         }
 
         GameObject[] slots = GameObject.FindGameObjectsWithTag("Slot");
