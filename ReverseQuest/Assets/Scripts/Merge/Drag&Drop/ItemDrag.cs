3cce9f3b6ea7a998cf9738956d38eaf4acbbdc28
lihtniht <114800709+Houchik@users.noreply.github.com>
2023-06-26 19:03:31 +0600

    safe-area-added

--- ReverseQuest/Assets/Scripts/Merge/Drag&Drop/ItemDrag.cs
+++ ReverseQuest/Assets/Scripts/Merge/Drag&Drop/ItemDrag.cs
@@ -1,29 +1,24 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public class ItemDrag : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
     private Canvas _canvas;
-
     private RectTransform _rectTransform;
     private CanvasGroup _canvasGroup;
-    private GameObject _SlotsPanel;
 
     private void Start()
     {
         _canvas = GameObject.Find("MergeCanvas").GetComponent<Canvas>();
         _rectTransform = GetComponent<RectTransform>();
         _canvasGroup = GetComponent<CanvasGroup>();
-        _SlotsPanel = GameObject.Find("SlotsPanel");
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        var slot = _rectTransform;
-        slot.SetParent(_canvas.transform);
-        //slot.transform.SetAsLastSibling(); //чтобы объект отрисовывался поверх других
-        _canvasGroup.blocksRaycasts = false; //чтобы лучи проходили сквозь объект
+        var slot = _rectTransform.parent;
+        slot.SetAsLastSibling(); //чтобы отрисовывался поверх других
+        _canvasGroup.blocksRaycasts = false; //чтобы лучи проходили сквозь
     }
 
     public void OnDrag(PointerEventData eventData)
--- ReverseQuest/Assets/Scripts/Merge/Grid Manager/FitType.cs
+++
@@ -1,9 +0,0 @@
-
-public enum FitType
-{
-    Uniform,
-    Width,
-    Height,
-    FixedRows,
-    FixedColumns
-}
